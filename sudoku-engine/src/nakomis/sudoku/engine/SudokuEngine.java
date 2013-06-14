ca6f02e91c9839d37faaede3ec68756764386a53
Martin Harris <github@nakomis.com>
2013-06-14 19:41:46 +0100

    Added basic preset digit entry

--- sudoku-engine/src/nakomis/sudoku/engine/SudokuEngine.java
+++ sudoku-engine/src/nakomis/sudoku/engine/SudokuEngine.java
@@ -1,13 +1,21 @@
 
 package nakomis.sudoku.engine;
 
+import java.util.Map;
+
 public class SudokuEngine {
-	public SudokuEngine() {
-		
+	private final Map<Map.Entry<Integer, Integer>, Integer> presetDigits;
+	
+	public SudokuEngine(final Map<Map.Entry<Integer,Integer>, Integer> presetDigits) {
+		this.presetDigits = presetDigits;
 	}
 
 	public void solve() {
 		// TODO Auto-generated method stub
 		
+	}
+
+	public Map<Map.Entry<Integer, Integer>, Integer> getPresetDigits() {
+		return presetDigits;
 	}
 }
--- sudoku-gui/src/nakomis/sudoku/gui/SudokuGui.java
+++ sudoku-gui/src/nakomis/sudoku/gui/SudokuGui.java
@@ -1,11 +1,39 @@
 package nakomis.sudoku.gui;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.text.MessageFormat;
+import java.util.AbstractMap;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import nakomis.sudoku.engine.SudokuEngine;
 
 public class SudokuGui {
 
-	public static void main(String[] args) {
-		SudokuEngine engine = new SudokuEngine();
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		List<String> input = new ArrayList<String>();
+		Map<Map.Entry<Integer,Integer>, Integer> presetDigits = new HashMap<Map.Entry<Integer,Integer>, Integer>();
+		for (int i = 0; i < 9; i++) {
+			System.out.println(MessageFormat.format("Input line #{0} as 9 digits (0 for blank)", i + 1));
+			String inputLine = br.readLine();
+			if (!inputLine.matches("^\\d{9}$")) {
+				System.out.println("Invalid input, application will exit");
+				return;
+			}
+			for (int j = 0; j < 9; j++) {
+				int digit = Character.getNumericValue(inputLine.charAt(j));
+				presetDigits.put(new AbstractMap.SimpleEntry<Integer, Integer>(j, i), digit);
+			}
+			input.add(inputLine);
+		}
+		br.close();
+		System.out.println(input);
+		SudokuEngine engine = new SudokuEngine(presetDigits);
 		engine.solve();
 	}
 
