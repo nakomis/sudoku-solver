fdf4f40711f01b9bbe4003a050a8116ec156961f
Martin Harris <github@nakomis.com>
2013-06-14 20:40:55 +0100

    Board initialization

--- sudoku-engine/src/nakomis/sudoku/engine/Board.java
+++ sudoku-engine/src/nakomis/sudoku/engine/Board.java
@@ -1,13 +1,53 @@
 package nakomis.sudoku.engine;
 
+import java.util.AbstractMap;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.Set;
 
 public class Board {
-
+	private final Set<Cell> cells;
+	private final Set<Group> groups;
+	
 	public Board(Map<Entry<Integer, Integer>, Integer> presetDigits) {
-		// TODO Auto-generated constructor stub
+		cells = new HashSet<Cell>();
+		groups = new HashSet<Group>();
+		List<Group> rows = new ArrayList<Group>();
+		List<Group> columns = new ArrayList<Group>();
+		List<Group> quadrants = new ArrayList<Group>();
+		for (int i = 0; i < 9; i++) {
+			rows.add(new Group());
+			columns.add(new Group());
+			quadrants.add(new Group());
+		}
+		
+		for (int x = 0; x < 9; x++) {
+			for (int y = 0; y < 9; y++) {
+				Integer preset = presetDigits.get(new AbstractMap.SimpleEntry<Integer, Integer>(x, y));
+				Cell cell;
+				if (preset == null) {
+					cell = new Cell();
+				} else {
+					cell = new Cell(preset);
+				}
+				cells.add(cell);
+				columns.get(x).add(cell);
+				rows.get(y).add(cell);
+				quadrants.get(findQuadrant(x, y)).add(cell);
+			}
+		}
+		
+		groups.addAll(rows);
+		groups.addAll(columns);
+		groups.addAll(quadrants);
+	}
+
+	private int findQuadrant(int x, int y) {
+		return ((int) y / 3) * 3 + x;
 	}
 
 	public void run() {
--- sudoku-engine/src/nakomis/sudoku/engine/Group.java
+++ sudoku-engine/src/nakomis/sudoku/engine/Group.java
@@ -2,4 +2,9 @@ package nakomis.sudoku.engine;
 
 public class Group {
 
+	public void add(Cell cell) {
+		// TODO Auto-generated method stub
+		
+	}
+
 }
